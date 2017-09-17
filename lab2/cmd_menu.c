a7802952e218558d0b2023adf27dab3de356ebdc
DesertSeagull <826086624@qq.com>
2017-09-17 17:42:32 +0800

    Update cmd_menu.c

--- lab2/cmd_menu.c
+++ lab2/cmd_menu.c
@@ -3,22 +3,27 @@
 #include<stdlib.h>
 #include<string.h>
 
+void help()
+{
+	printf("enter help [-commandname] for details");
+}
+void 
 int main()
 {
     char cmd[128];
     while(1)
     {
         scanf("%s",cmd);
-	if(strcmp(cmd,"help"))
+	if(strcmp(cmd,"help")==0)
 	    help();
-	else if(strcmp(cmd,""))
-        else if(strcmp(cmd,""))
-	else if(strcmp(cmd,""))
-	else if(strcmp(cmd,""))
-	else if(strcmp(cmd,""))
-	else if(strcmp(cmd,""))
-	else if(strcmp(cmd,""))
-         
+	else if(strcmp(cmd,"")==0)
+        else if(strcmp(cmd,"")==0)
+	else if(strcmp(cmd,"")==0)
+	else if(strcmp(cmd,"")==0)
+	else if(strcmp(cmd,"")==0)
+	else if(strcmp(cmd,"")==0)
+	else if(strcmp(cmd,"exit")==0)
+            exit(); 
 	    	
 		  
     }
