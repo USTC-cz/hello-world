fb1b1250b10a92e551429a18e32cbdd602786743
826086624 <826086624@qq.com>
2017-09-17 17:28:56 +0800

    cmd_menu

--- lab2/cmd_menu.c
+++ lab2/cmd_menu.c
@@ -8,6 +8,18 @@ int main()
     char cmd[128];
     while(1)
     {
+        scanf("%s",cmd);
+	if(strcmp(cmd,"help"))
+	    help();
+	else if(strcmp(cmd,""))
+        else if(strcmp(cmd,""))
+	else if(strcmp(cmd,""))
+	else if(strcmp(cmd,""))
+	else if(strcmp(cmd,""))
+	else if(strcmp(cmd,""))
+	else if(strcmp(cmd,""))
          
+	    	
+		  
     }
 }
