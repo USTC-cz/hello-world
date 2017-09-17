00046e40a584b037597e4987b5b2049f126b4392
DesertSeagull <826086624@qq.com>
2017-09-17 17:57:38 +0800

    Update cmd_menu.c

--- lab2/cmd_menu.c
+++ lab2/cmd_menu.c
@@ -7,7 +7,15 @@ void help()
 {
 	printf("enter help [-commandname] for details");
 }
-void 
+void hello()
+{
+	printf("hello,user");
+}
+void add()
+{
+	int a,b;
+	printf("%d%d",a+b);
+}
 int main()
 {
     char cmd[128];
@@ -16,14 +24,20 @@ int main()
         scanf("%s",cmd);
 	if(strcmp(cmd,"help")==0)
 	    help();
-	else if(strcmp(cmd,"")==0)
-        else if(strcmp(cmd,"")==0)
-	else if(strcmp(cmd,"")==0)
-	else if(strcmp(cmd,"")==0)
-	else if(strcmp(cmd,"")==0)
-	else if(strcmp(cmd,"")==0)
+	else if(strcmp(cmd,"abs")==0)
+	    abs();
+        else if(strcmp(cmd,"pow")==0)
+	    pow();
+	else if(strcmp(cmd,"time")==0)
+	    time();
+	else if(strcmp(cmd,"hello")==0)
+	    hello();
+	else if(strcmp(cmd,"add")==0)
+	    add();	
 	else if(strcmp(cmd,"exit")==0)
-            exit(); 
+            exit(0); 
+	else 
+	    printf("commond not found");
 	    	
 		  
     }
