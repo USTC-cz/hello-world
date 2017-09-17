c9fbd100d82d66f73a2acbf215472875b5df3a62
826086624 <826086624@qq.com>
2017-09-17 18:16:51 +0800

    menu

--- lab2/cmd_menu.c
+++ lab2/cmd_menu.c
@@ -1,21 +1,37 @@
 #include<stdio.h>
-#include<math.h>
 #include<stdlib.h>
 #include<string.h>
 
 void help()
 {
-	printf("enter help [-commandname] for details");
+    printf("enter help [-commandname] for details");
 }
 void hello()
 {
-	printf("hello,user");
+    printf("hello,user");
 }
 void add()
 {
-	int a,b;
-	printf("%d%d",a+b);
+    int a,b;
+    printf("%d+%d",a,b);
 }
+void oppnum()
+{
+    int a;
+    printf("%d",-a);	
+}
+void compare();
+{
+    int a,b;
+    if(a<b)
+	printf("a<b");
+    else if(a=b)
+	printf("a=b");
+    else
+	printf("a>b");
+}
+
+
 int main()
 {
     char cmd[128];
@@ -24,9 +40,9 @@ int main()
         scanf("%s",cmd);
 	if(strcmp(cmd,"help")==0)
 	    help();
-	else if(strcmp(cmd,"abs")==0)
+	else if(strcmp(cmd,"compare")==0)
 	    abs();
-        else if(strcmp(cmd,"pow")==0)
+        else if(strcmp(cmd,"oppnum")==0)
 	    pow();
 	else if(strcmp(cmd,"time")==0)
 	    time();
@@ -37,8 +53,7 @@ int main()
 	else if(strcmp(cmd,"exit")==0)
             exit(0); 
 	else 
-	    printf("commond not found");
-	    	
-		  
-    }
+	    printf("commond not found");		  
+     }
+    return 0;
 }
