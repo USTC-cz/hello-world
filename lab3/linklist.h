bc5ee335c3422b8ae55a8bc41423bc0378fa904a
DesertSeagull <826086624@qq.com>
2017-10-07 23:23:24 +0800

    Update linklist.h

--- lab3/linklist.h
+++ lab3/linklist.h
@@ -1,1 +1,23 @@
+/**********************************************************/
+/* Copyright (C) 2017 Caizhou.All rights reserved.        */
+/*                                                        */
+/* FILE NAME             : linklist.h                     */
+/* AUTHOR                : Caizhou                        */
+/* LANGUAGE              : C                              */
+/**********************************************************/
+
+
+#ifndef _LINKLIST_H_
+#define _LINKLIST_H_
+typedef struct DataNode
+{
+	char *cmd;
+	char *info;
+	int (*handler)();
+	struct DataNode *next;
+}tDataNode;
+tDataNode *FindCmd(tDataNode *head,char *cmd);
+int ShowAllCmd(tDataNode);
+#endif
+
 
